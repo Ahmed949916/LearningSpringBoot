95dc0a1d4fe277fdca4564977f205236f5b60ce0
Ahmed949916 <ahmed40152@gmail.com>
2025-07-18 12:39:22 +0500

    feat:added password hashing

--- NewsProjectV1-main/src/main/java/org/redmath/config/ApiConfiguration.java
+++ NewsProjectV1-main/src/main/java/org/redmath/config/ApiConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
@@ -19,6 +21,11 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class ApiConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
