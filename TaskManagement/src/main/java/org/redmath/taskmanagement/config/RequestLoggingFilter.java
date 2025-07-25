52e3afc1694bb99c8c7e7dd838c31811d713b842
Ahmed949916 <ahmed40152@gmail.com>
2025-07-25 12:51:38 +0500

    feat:requestLogger

---
+++ TaskManagement/src/main/java/org/redmath/taskmanagement/config/RequestLoggingFilter.java
@@ -0,0 +1,28 @@
+
+package org.redmath.taskmanagement.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        log.info("➡️ Incoming request: {} {}", request.getMethod(), request.getRequestURI());
+        filterChain.doFilter(request, response);
+    }
+}
